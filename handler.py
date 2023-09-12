73ed37f33dfb171a1069887e3e0ebf985fdb298a
Sean Welch <seanw51199@gmail.com>
2023-09-12 15:48:07 +0100

    successfully connected to and queried database

--- handler.py
+++ handler.py
@@ -1,12 +1,42 @@
 import os
 import json
 import boto3
-import pyodbc
+import psycopg2
+from dotenv import load_dotenv
+from typing import Any, Dict
+
+load_dotenv()
 
 
 def hello(event, context):
-    body = {"message": "Hi there, this is a test lambda function", "input": event}
+    cursor = None
+    connection = None
 
-    response = {"statusCode": 200, "body": json.dumps(body)}
+    try:
+        connection = psycopg2.connect(
+            host=os.environ["PGHOST"],
+            dbname=os.environ["PGDATABASE"],
+            user=os.environ["PGUSER"],
+            password=os.environ["PGPASSWORD"],
+            port=os.environ["PGPORT"],
+        )
 
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM "Product"')
+        result = cursor.fetchall()
+
+        body = {"message": "Query executed", "data": result, "input": event}
+        status_code = 200
+
+    except Exception as error:
+        body = {"message": str(error)}
+        status_code = 500
+
+    finally:
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
+
+    response = {"statusCode": status_code, "body": json.dumps(body)}
     return response
