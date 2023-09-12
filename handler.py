eaefd742cce6ab9fd401ac98ecf474c9f1b00d8a
Sean Welch <seanw51199@gmail.com>
2023-09-12 14:45:48 +0100

    installed some dependancies

--- handler.py
+++ handler.py
@@ -1,24 +1,12 @@
+import os
 import json
+import boto3
+import pyodbc
 
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    body = {"message": "Hi there, this is a test lambda function", "input": event}
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    response = {"statusCode": 200, "body": json.dumps(body)}
 
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
